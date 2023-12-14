80397f1bb7631b3b6b24d3f1777647fc80c58b1a
SebD <sebastian.detering@gmail.com>
2023-12-14 11:31:35 -0800

    Initial Commit

---
+++ Chroma Snowflake Fractal/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Chroma Snowflake Fractal
+//
+//  Created by sebi d on 12/14/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Chroma Snowflake Fractal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Chroma Snowflake Fractal
+//
+//  Created by sebi d on 12/14/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
